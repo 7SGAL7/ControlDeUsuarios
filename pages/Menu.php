48cdc7a89e7236a587fa4a7f4d46c1267578bc4a
Samir Aes <samirgonzaloaeslopez@outlook.com>
2025-01-22 21:18:50 -0600

    agregado de boton para cierre de menu

--- pages/Menu.php
+++ pages/Menu.php
@@ -44,6 +44,7 @@ body {
     <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
       <div class="offcanvas-header">
         <h5 class="offcanvas-title" id="offcanvasNavbarLabel">MENU</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       <div class="offcanvas-body">
         <div class="d-flex align-items-start flex-column mb-3">
